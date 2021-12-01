4288b83f27a76200abb4a608cce5f53a2599076f
Brad Garropy <bradgarropy@gmail.com>
2021-11-30 20:54:50 -0600

    done.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
 import SEO from "@bradgarropy/next-seo"
 import Layout from "components/Layout"
 import {FC, useState} from "react"
+import {motion, AnimatePresence} from "framer-motion"
+
+const boxAnimation = {
+    key: "box",
+    initial: {
+        y: "50%",
+        opacity: 0,
+        scale: 0.5,
+    },
+    animate: {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+    },
+    exit: {
+        y: "50%",
+        opacity: 0,
+        transition: {
+            duration: 0.1,
+        },
+    },
+    transition: {
+        duration: 0.2,
+        ease: "easeOut",
+    },
+}
 
 const IndexPage: FC = () => {
     const [isShown, setIsShown] = useState(true)
@@ -14,7 +40,10 @@ const IndexPage: FC = () => {
             <SEO title="next starter" />
 
             <button onClick={onClick}>{isShown ? "Hide" : "Show"}</button>
-            {isShown && <div className="box"></div>}
+
+            <AnimatePresence>
+                {isShown && <motion.div {...boxAnimation} className="box" />}
+            </AnimatePresence>
         </Layout>
     )
 }
